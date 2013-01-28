ac54702acef340292b69c18a7a8e313a4924018f
mub <m.uberti@agomir.com>
2013-01-28 14:07:46 +0100

    Inizio voice recognition

--- AttachNote/src/com/agomir/attachnote/MainActivity.java
+++ AttachNote/src/com/agomir/attachnote/MainActivity.java
@@ -1,11 +1,11 @@
 package com.agomir.attachnote;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import com.agomir.attachnote.listeners.ShakeListener;
 import com.agomir.attachnote.utils.FileUtils;
 import com.agomir.attachnote.view.FingerPaintDrawableView;
-
 
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,16 +14,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 738392362;
+	
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
 	private ShakeListener shakeListener;
@@ -55,6 +64,41 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	 /**
+     * Fire an intent to start the speech recognition activity.
+     */
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    /**
+     * Handle the results from the recognition activity.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Fill the list view with the strings the recognizer thought it could have heard
+            ArrayList<String> matches = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            
+            for(String s: matches) {
+            	if(s.toLowerCase().contains("verde")) {
+            		fingerPaintView.setColor(Color.GREEN);
+            	}else if(s.toLowerCase().contains("rosso")) {
+            		fingerPaintView.setColor(Color.RED);
+            	}else if(s.toLowerCase().contains("blu")) {
+            		fingerPaintView.setColor(Color.BLUE);
+            	}
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,7 +109,17 @@ public class MainActivity extends Activity {
 		fingerRelativeParams.addRule(RelativeLayout.CENTER_IN_PARENT);
 		
         RelativeLayout root = (RelativeLayout)findViewById(R.id.root_layout);
-        root.addView(fingerPaintView,fingerRelativeParams);
+        root.addView(fingerPaintView,0,fingerRelativeParams);
+        
+        //Bottone per il riconoscimento vocale
+        Button btnVoice = (Button)findViewById(R.id.voiceRecognition);
+        btnVoice.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startVoiceRecognitionActivity();
+			}
+		});
         
         //Se in input abbiamo un file mandato con SEND
     	if(getIntent().getAction().equalsIgnoreCase(Intent.ACTION_SEND)) {
@@ -86,7 +140,6 @@ public class MainActivity extends Activity {
 					return;
 				}
 	    		
-	    		//Leggo solo le dimensioni dell'immagine
 	    	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
 	    	    bmOptions.inJustDecodeBounds = false;
 	    	    Bitmap bitmap = BitmapFactory.decodeFile(filePath, bmOptions);
